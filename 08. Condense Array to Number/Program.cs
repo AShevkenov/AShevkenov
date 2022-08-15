9f0cacd0fbb63dd57a4cf080a747208ff136f63d
Alexander Shevkenov <ashevkenov@gmail.com>
2022-08-15 17:12:47 +0300

    Adding Arrays Exercise FUND C#

---
+++ 08. Condense Array to Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray(); // 2
+            int[] condensed = new int[numbers.Length - 1]; // 2-1 => 1
+
+
+            while (numbers.Length > 1)
+            {
+
+                for (int i = 0; i < numbers.Length - 1; i++) // i = 1; 1 < 2
+                {
+                    condensed[i] = numbers[i] + numbers[i + 1]; // 2+10 = 12; 10+3 = 13;
+
+                    if (i == numbers.Length - 2)
+                    {
+                        numbers = condensed;
+                        condensed = new int[numbers.Length - 1];
+                    }
+                }
+            }
+            Console.WriteLine(numbers[0]);
+
+        }
+    }
+}
